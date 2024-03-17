b69e54a24a7f92350ccac4bcf5e4ef51bc9a9c02
Adlai M.B <adlaimb@gmail.com>
2024-03-17 13:30:52 -0700

    create triggers and indexes

--- sql/src/triggers.sql
+++ sql/src/triggers.sql
@@ -1,4 +1,5 @@
 DROP TRIGGER IF EXISTS trg_product_update ON Product;
+DROP TRIGGER IF EXISTS trg_new_order ON Orders;
 
 CREATE OR REPLACE LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION product_update()
@@ -12,7 +13,24 @@ END;
 $BODY$
 LANGUAGE plpgsql VOLATILE;
 
+CREATE OR REPLACE LANGUAGE plpgsql;
+CREATE OR REPLACE FUNCTION product_units_update()
+RETURNS "trigger" AS 
+$BODY$
+DECLARE 
+  oldUnits  integer;
+BEGIN
+  oldUnits := (SELECT numberOfUnits FROM Product WHERE storeID = NEW.storeID AND productName = NEW.productName);
+  UPDATE Product SET numberOfUnits = oldUnits - NEW.unitsOrdered WHERE storeID = NEW.storeID AND productname = NEW.productName;
+  RETURN NEW;
+END;
+$BODY$
+LANGUAGE plpgsql VOLATILE;
 
 CREATE TRIGGER trg_product_update
 AFTER UPDATE ON Product
 FOR EACH ROW EXECUTE PROCEDURE product_update();
+
+CREATE TRIGGER trg_new_order
+AFTER INSERT ON Orders
+FOR EACH ROW EXECUTE PROCEDURE product_units_update();
